31fad2472cafd7a69530e5c4b1e07b1f88e2db5f
Ildar Galeev <KeinAsylum@gmail.com>
2016-08-30 15:41:34 +0300

    FE-40: Used only required thrift modules

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,12 +1,13 @@
-var thrift = require('./thrift/index');
+var httpConnection = require('./thrift/http_connection');
+var TBufferedTransport = require('./thrift/buffered_transport');
+var TBinaryProtocol = require('./thrift/binary_protocol');
 var Repository = require('./gen-nodejs/Repository');
 
 (function () {
-    var connection = thrift.createHttpConnection('localhost', 9000, {
-        transport: thrift.TBufferedTransport(),
-        protocol: thrift.TBinaryProtocol(),
+    var connection = httpConnection.createHttpConnection('localhost', 9000, {
+        transport: TBufferedTransport(),
+        protocol: TBinaryProtocol(),
         path: '/v1/domain/repository'
     });
-
-    this.repository = thrift.createHttpClient(Repository, connection);
+    this.repository = httpConnection.createHttpClient(Repository, connection);
 }).call(window || {});
