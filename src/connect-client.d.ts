6518e78ad5e957bc044744909af5fd772a273310
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-01-10 21:08:21 +0300

    publish @vality/woody

--- src/connect-client.d.ts
+++ src/connect-client.d.ts
@@ -1,5 +1,5 @@
 import {ConnectOptions} from "./connect-options";
 
-declare module '@rbkmoney/woody_js' {
+declare module '@vality/woody' {
     export default function connectClient(host: string, port: string, path: string, genClient: object, connectOptions: ConnectOptions, errorCb: Function): object;
 }
