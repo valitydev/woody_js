fedeaf6ccba96a9574bc54849df58f0281fad327
Ildar Galeev <KeinAsylum@gmail.com>
2023-11-01 17:55:01 +0300

    Add vite config

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import commonjs from 'vite-plugin-commonjs';
+
+export default defineConfig({
+    plugins: [commonjs()],
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+        minify: true,
+        lib: {
+            entry: './src/connect-client.js',
+            formats: ['es'],
+            fileName: 'connect-client',
+        },
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            https: 'https-browserify',
+            http: 'stream-http',
+        },
+    },
+});
