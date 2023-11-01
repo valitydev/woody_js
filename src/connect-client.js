9c82854af2c8b2f4eabae29de3667bf4f35cdd0e
Ildar Galeev <KeinAsylum@gmail.com>
2023-11-01 18:23:32 +0300

    Add process dep

--- src/connect-client.js
+++ src/connect-client.js
@@ -1,12 +1,19 @@
+require('process/browser');
+
 const TBinaryProtocol = require('./client/binary_protocol');
 const TBufferedTransport = require('./client/buffered_transport');
 const httpConnection = require('./client/http_connection');
 
 export default function connectClient(host, port, path, genClient, options = {}, errorCb) {
-    const connection = httpConnection.createHttpConnection(host, port, Object.assign(options, {
-        transport: TBufferedTransport,
-        protocol: TBinaryProtocol,
-        path
-    }), errorCb);
+    const connection = httpConnection.createHttpConnection(
+        host,
+        port,
+        Object.assign(options, {
+            transport: TBufferedTransport,
+            protocol: TBinaryProtocol,
+            path,
+        }),
+        errorCb
+    );
     return httpConnection.createHttpClient(genClient, connection);
 }
